658ab27fad26ac13795c1fc8533b0e1b0906fb62
Eric Broch <ebroch@whitehorsetc.com>
2020-01-01 21:10:00 -0700

    Update qt_install_centos8.sh

--- qt_install_centos8.sh
+++ qt_install_centos8.sh
@@ -45,7 +45,7 @@ if [ -z "$password" ]; then
    echo "Empty password, exiting..."
    exit 1
 fi
-echo "\n"
+echo -e "\n"
 MYSQLPW=$password
 credfile=~/sql.cnf
 echo -e "[client]\nuser=root\npassword=$MYSQLPW\nhost=localhost" > $credfile
