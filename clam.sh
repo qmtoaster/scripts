95c7c575bbea1df7914e591aaad42d964dab7a93
Eric Broch <ebroch@whitehorsetc.com>
2020-04-08 16:09:25 -0600

    Create clam.sh

---
+++ clam.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+systemctl stop clamav-daemon clamav-socket
+
+rpm -ev --nodeps clamav
+
+yum install patch clamav-update clamd -y
+
+printf '%s\n' \
+'# Run the freshclam as daemon' \
+'[Unit]' \
+'Description = freshclam scanner' \
+'After = network.target' \
+'' \
+'[Service]' \
+'Type = forking' \
+'ExecStart = /usr/bin/freshclam -d -c 4' \
+'Restart = on-failure' \
+'PrivateTmp = true' \
+'' \
+'[Install]' \
+'WantedBy=multi-user.target' \
+> /usr/lib/systemd/system/clamav-freshclam.service
+
+chown clamscan:root /var/qmail/simscan
+chown clamscan:root /var/qmail/bin/simscan
+chmod 0750 /var/qmail/simscan
+chmod 4711 /var/qmail/bin/simscan
+mkdir /var/log/clamd
+chown -R clamscan:clamscan /var/log/clamd
+
+wget http://www.qmailtoaster.org/clamd.conf.diff
+
+patch /etc/clamd.d/scan.conf clamd.conf.diff
+
+systemctl start clamd@scan freshclam
+systemctl enable clamd@scan freshclam
+
+sed -i 's/CLAMD=/CLAMD=clamd@scan/' /usr/bin/toaststat
+
+toaststat
