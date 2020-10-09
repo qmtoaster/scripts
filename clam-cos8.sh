40868b32056d6b55a223cd1175df01ab51331d59
Eric Broch <ebroch@whitehorsetc.com>
2020-10-09 08:54:51 -0600

    Update clam-cos8.sh

--- clam-cos8.sh
+++ clam-cos8.sh
@@ -10,19 +10,13 @@ yum --disablerepo=qmt-current \
     --disablerepo=qmt-devel \
     install clamav clamav-update clamd -y
 
-curl -o /usr/lib/systemd/system/clamd@scan.service  https://raw.githubusercontent.com/qmtoaster/scripts/master/clamd@scan.service
-curl -o /etc/clamd.d/scan.conf https://raw.githubusercontent.com/qmtoaster/scripts/master/scan.conf
-
 chown clamscan:root /var/qmail/simscan
 chown clamscan:root /var/qmail/bin/simscan
 chmod 0750 /var/qmail/simscan
 chmod 4711 /var/qmail/bin/simscan
-[ ! -d /var/log/clamd ] && mkdir /var/log/clamd
-[ ! -d /var/log/clamav ] && mkdir /var/log/clamav
-chown -R clamscan:clamscan /var/log/clamd
-chown -R clamupdate:clamupdate /var/log/clamav
 chown -R clamupdate:clamupdate /var/lib/clamav
-sed -i 's/#UpdateLogFile \/var\/log\/freshclam.log/UpdateLogFile \/var\/log\/clamav\/freshclam.log/g; s/#LogFileMaxSize/LogFileMaxSize/g; s/#LogTime/LogTime/g; s/#LogVerbose/LogVerbose/g; s/#LogRotate/LogRotate/g' /etc/freshclam.conf
+
+sed -i 's/^#LocalSocket /LocalSocket /'  /etc/clamd.d/scan.conf
 
 freshclam
 systemctl enable --now   clamd@scan clamav-freshclam
