8c58a313977b619a065538f44266c87d1c649026
Eric Broch <ebroch@whitehorsetc.com>
2023-04-19 18:14:33 -0600

    Update qt_install_cos8.sh

--- qt_install_cos8.sh
+++ qt_install_cos8.sh
@@ -219,7 +219,7 @@ sed -i -e 's/Define aclnet "127.0.0.1"/Define aclnet "192.168.2.0\/24 192.168.9.
  mysql --defaults-extra-file=$credfile -e "GRANT ALL PRIVILEGES ON roundcube.* TO roundcube@localhost"
  mysql --defaults-extra-file=$credfile roundcube < /usr/share/roundcubemail/SQL/mysql.initial.sql
  cp -p /etc/httpd/conf.d/roundcubemail.conf /etc/httpd/conf.d/roundcubemail.conf.bak && \
- wget -O /etc/roundcubemail/config.inc.php http://www.qmailtoaster.org/rc.default.config && \
+ wget -O /etc/roundcubemail/config.inc.php http://www.qmailtoaster.org/rc.default.config.rhelgt7 && \
  wget -O /etc/httpd/conf.d/roundcubemail.conf http://www.qmailtoaster.org/rc.httpd.config
  sed -i 's/\;date.timezone.*/date.timezone = "America\/Denver"/' /etc/php.ini | sleep 2 | cat /etc/php.ini | grep date.timezone.*=
  systemctl restart httpd
